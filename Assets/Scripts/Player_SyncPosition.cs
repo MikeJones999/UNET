138780b060f89f0f0e983678514a0d1adda8fe7d
mikiejones <mikiej_boy@hotmail.com>
2015-11-06 00:49:28 +0000

    Historic Lerping added Added the ability to turn on or off historic lerping in the event of latency
    The historic lerping is an list of vector3 - waypoints that the player has moved through. These are sent in retrospec to other clients so on their scene they see the player move through way points with a ms delay. almost like a replay - this provides a smoother movement on the screen. however the effects are unknown when using shooting and detection.
    
    Lerping of rotation has as yet not been added

--- Assets/Scripts/Player_SyncPosition.cs
+++ Assets/Scripts/Player_SyncPosition.cs
@@ -1,23 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
-[NetworkSettings(channel = 0, sendInterval = 0.033f)]
+[NetworkSettings(channel = 0, sendInterval = 0.01f)]
 public class Player_SyncPosition : NetworkBehaviour
 {
 
     //variable gets synched across network - Server automatically transmit this value to all clients upon changing
-    [SyncVar]
+    [SyncVar (hook = "SyncPositionValues")]
     private Vector3 syncPos;
 
 
-    [SerializeField] Transform myTransform;    
+    [SerializeField] Transform myTransform;
     [SerializeField] float lerpRate = 15;
+ 
 
+
+    
     //this holds the players last position - used to update network only when player has moved 0.5 metres.
     private Vector3 lastPos;
     private float threshold = 0.4f;
 
+
+
+    //handle some of the latency - create a list holds waypoints of player's movement - this list get sent to other clients who will use it to trace players movements
+    //it provides a historic movemet of teh players location but the clienst dont know this and to them this is current
+    private List<Vector3> syncPosList = new List<Vector3>();
+    private float normalLerpRate = 18.0f;
+    private float fasterLerpRate = 27.0f;
+    [SerializeField] private bool useHistoricLerp = false;
+    private float closeEnough = 0.11f; //0.1 metres
+
+    void Start()
+    {
+        lerpRate = normalLerpRate;
+    }
+    
 
     void Update()
     {
@@ -37,11 +56,18 @@ public class Player_SyncPosition : NetworkBehaviour
     //smoothing out position data of other players not local to us
     void LerpPosition()
     {
-        if(!isLocalPlayer)
+        if (!isLocalPlayer)
         {
-            //my charachter will lerp between locations on other players computers
-            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
-           
+            if(useHistoricLerp)
+            {
+                HistoricalLerping();
+            }
+            else
+            {
+                //call standard lerping method
+                StandardLerping();
+            }
+
         }
     }
 
@@ -66,6 +92,55 @@ public class Player_SyncPosition : NetworkBehaviour
             lastPos = myTransform.position;
         }
     }
+
+    [Client]
+    void SyncPositionValues(Vector3 latestPos)
+    {
+        syncPos = latestPos;
+        syncPosList.Add(syncPos);
+    }
+
+
+    void StandardLerping()
+    {
+        if (!isLocalPlayer)
+        {
+            //my charachter will lerp between locations on other players computers
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+
+    //hold waypoints in list
+    void HistoricalLerping()
+    {
+        //if empty - pointless doing anything
+        if(syncPosList.Count > 0)
+        {
+            //interpolate towards the first position in the list
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPosList[0], Time.deltaTime * lerpRate);
+
+            if(Vector3.Distance(myTransform.position, syncPosList[0]) < closeEnough)
+            {
+                //remove entry from the list as player has already reached there
+                syncPosList.RemoveAt(0);
+            }
+
+
+            //player moved towards the waypoint a little quicker - inturn this removes an element from the array as teh waypoint is reached quicker
+            if(syncPosList.Count > 10)
+            {
+                lerpRate = fasterLerpRate;
+            }
+            else
+            {
+                lerpRate = normalLerpRate;
+            }
+
+            Debug.Log("HistLerp " + syncPosList.Count.ToString());
+        }
+    }
+
 
 }
 
